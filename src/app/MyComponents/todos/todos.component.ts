5cabb2f248f020636b6bf85865d9e4a3d285b247
urmil404 <urmil8989@gmail.com>
2022-12-22 02:18:56 -0800

    add and delete working

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
-
+import { Todo } from '../../Todo';
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent {
-
+  todos: Todo[];
+  constructor() {
+    this.todos = [
+      {
+        sno: 1,
+        title: 'todo title 1',
+        desc: 'todo desc 1',
+        active: true,
+      },
+      {
+        sno: 2,
+        title: 'todo title 2',
+        desc: 'todo desc 2',
+        active: true,
+      },
+      {
+        sno: 3,
+        title: 'todo title 3',
+        desc: 'todo desc 3',
+        active: true,
+      },
+      {
+        sno: 4,
+        title: 'todo title 4',
+        desc: 'todo desc 4',
+        active: true,
+      },
+    ];
+  }
+  deleteTodo(todo: Todo) {
+    console.log(todo);
+	  const index = this.todos.indexOf(todo);
+	  this.todos.splice(index, 1);
+  }
+  addTodo(todo: Todo) {
+    console.log(todo);
+    this.todos.push(todo);
+  }
 }
